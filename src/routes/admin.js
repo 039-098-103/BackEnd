c1d8394a3fee2daa9947cd3519d5b5031e4c3e23
zyct <thanyaluck.yict@mail.kmutt.ac.th>
2021-09-20 18:41:04 +0700

    [update] exception handling

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -14,82 +14,129 @@ router.get('/getStaffList', async(req, res) => {
     if (req.payload.role != 'Admin') {
         res.status(401).json("You don't have Permission!")
     } else {
-        const staffList = await worker.findMany({
-            select: {
-                username: true,
-                firstName: true,
-                lastName: true,
-                DOB: true,
-            },
-            where: {
-                position: 'Staff'
+        try {
+            const staffList = await worker.findMany({
+                select: {
+                    username: true,
+                    firstName: true,
+                    lastName: true,
+                    DOB: true,
+                },
+                where: {
+                    position: 'Staff'
+                }
+            })
+            for (let i in staffList) {
+                staffList[i].DOB = formatDate(staffList[i].DOB)
             }
-        })
-        for (let i in staffList) {
-            staffList[i].DOB = formatDate(staffList[i].DOB)
+            res.send(staffList)
+        } catch (err) {
+            res.status(500).send("Something went wrong!")
         }
-        res.send(staffList)
     }
 })
 
 router.post('/addStaff', upload.single('data'), async(req, res) => {
     if (req.payload.role != 'Admin') {
-        res.status(401).json("You don't have Permission!")
+        res.status(401).send("You don't have Permission!")
     } else {
-        const data = JSON.parse(fs.readFileSync('./tmp/data.json', 'utf-8'));
-        await worker.create({
-            data: {
-                username: data.username,
-                password: await encryptPwd(data.password),
-                firstName: data.firstName,
-                lastName: data.lastName,
-                DOB: data.DOB,
-                position: data.position
+        try {
+            const data = JSON.parse(fs.readFileSync('./tmp/data.json', 'utf-8'));
+            //check if username duplicate
+            const isDuplicate = await findUser(data.username)
+            if (isDuplicate) {
+                res.status(400).send("Username already exists!")
+            } else {
+                await worker.create({
+                    data: {
+                        username: data.username,
+                        password: await encryptPwd(data.password),
+                        firstName: data.firstName,
+                        lastName: data.lastName,
+                        DOB: data.DOB,
+                        position: data.position
+                    }
+                })
+                res.send(`Staff ${data.username} has been created!`)
             }
-        })
-
-        res.send(`Staff ${data.username} has been created!`)
+        } catch (err) {
+            res.send(400).send("Could not add Staff!")
+        }
         fs.unlinkSync('./tmp/data.json')
     }
+
+
 })
 
 router.delete('/delete/:username', async(req, res) => {
     if (req.payload.role != 'Admin') {
-        res.status(401).json("You don't have Permission!");
+        res.status(401).send("You don't have Permission!");
     } else {
         const { username } = req.params
-        await worker.delete({
-            where: {
-                username: username
+        if (!username) {
+            res.send(400).send("Username is empty.")
+        } else {
+            try {
+                //check if username exists
+                const isExists = await findUser(username);
+                if (!isExists) {
+                    res.status(400).send("Username not found!")
+                } else {
+                    await worker.delete({
+                        where: {
+                            username: username
+                        }
+                    })
+                    res.status(200).send(`Staff ${username} has been deleted!`)
+                }
+            } catch (err) {
+                res.send(503).send("Could not delete the Staff!")
             }
-        })
-        res.status(200).send(`Staff ${username} has been deleted!`)
-    }
 
+        }
+    }
 
 })
 
 router.patch('/update', upload.single('data'), async(req, res) => {
     if (req.payload.role != 'Admin') {
-        res.status(401).json("You don't have Permission!")
+        res.status(401).send("You don't have Permission!")
     } else {
-        const data = JSON.parse(fs.readFileSync('./tmp/data.json', 'utf-8'))
+        try {
+            const data = JSON.parse(fs.readFileSync('./tmp/data.json', 'utf-8'))
 
-        await worker.updateMany({
-            data: {
-                username: data.username,
-                password: await encryptPwd(data.password),
-                firstName: data.firstName,
-                lastName: data.lastName,
-                DOB: data.DOB
-            },
-            where: {
-                position: 'Admin'
-            }
-        })
-        res.status(200).send(`Admin information has been updated!`)
+            await worker.updateMany({
+                data: {
+                    username: data.username,
+                    password: await encryptPwd(data.password),
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    DOB: data.DOB
+                },
+                where: {
+                    position: 'Admin'
+                }
+            })
+            res.status(200).send(`Admin information has been updated!`)
+
+        } catch (err) {
+            res.status(400).send('Could not update!')
+        }
         fs.unlinkSync('./tmp/data.json')
     }
 })
+
+async function findUser(username) {
+    const res = await worker.findUnique({
+        where: {
+            username: username
+        }
+    })
+    if (res) {
+        return true
+    } else {
+        return false
+    }
+}
 
 module.exports = router;
--- src/services/workerAuth.js
+++ src/services/workerAuth.js
@@ -7,32 +7,41 @@ const { signAccessToken } = require('../middleware/accessToken')
 
 router.post('/', async(req, res) => {
     const { username, password } = req.body
-    const result = await worker.findMany({
-        select: {
-            username: true,
-            password: true,
-            position: true
-        },
-        where: {
-            username: username
-        }
-    })
-
-    const pass = result[0].password
-    const pos = result[0].position
-    if (pass) {
-        const resCode = await comparePwd(password, pass)
-        if (resCode == 403) {
-            res.status(403).json("Wrong Password!")
-        }
-        if (resCode == 200) {
-            const token = await signAccessToken(username, pos)
-            res.status(200).send(token)
-        } else {
-            res.status(401).json("You don't have permission!")
-        }
+    if (!username || !password) {
+        res.status(400).send("Username or Password is empty!")
     } else {
-        res.status(404).json("User not found!")
+
+        try {
+            const result = await worker.findMany({
+                select: {
+                    username: true,
+                    password: true,
+                    position: true
+                },
+                where: {
+                    username: username
+                }
+            })
+
+            const pass = result[0].password
+            const pos = result[0].position
+            if (pass) {
+                const resCode = await comparePwd(password, pass)
+                if (resCode == 403) {
+                    res.status(403).send("Wrong Password!")
+                }
+                if (resCode == 200) {
+                    const token = await signAccessToken(username, pos)
+                    res.status(200).send(token)
+                } else {
+                    res.status(401).send("You don't have permission!")
+                }
+            } else {
+                res.status(404).send("User not found!")
+            }
+        } catch (err) {
+            res.status(400).send("Could not login!")
+        }
     }
 })
 
