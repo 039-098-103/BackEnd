a4072fa2d2ca1a9b2e8d1036e7eabaa77b572246
zyct <thanyaluck.yict@mail.kmutt.ac.th>
2021-09-21 21:28:10 +0700

    [update] CORs config

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ const app = express();
 
 const { authToken } = require('./middleware/accessToken')
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "http://52.187.115.71")
+    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    res.header('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE")
+    next();
+})
+
 app.use(express.json())
 app.use('/auth', require('./services/workerAuth'))
 app.use('/admin', authToken, require('./routes/admin'))
