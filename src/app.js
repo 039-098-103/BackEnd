d6d066f2eb465301ac7095fed251a9586ce9177f
zyct <thanyaluck.yict@mail.kmutt.ac.th>
2021-09-30 19:14:00 +0700

    [update] change allow origin

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 const { authToken } = require('./middleware/accessToken')
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "http://frontend")
+    res.header('Access-Control-Allow-Origin', "http://52.187.115.71")
     res.header('Access-Control-Allow-Headers', "*")
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE")
